734bbc9d8c7cc1817b10b0777e0036e05dcad564
Muhammad Hassan Raza <mhassanr715@gmail.com>
2026-07-22 18:25:05 +0500

    Update userContoller.js

--- controller/userContoller.js
+++ controller/userContoller.js
@@ -114,8 +114,9 @@ const loginUser = async (req, res) => {
 
   res.cookie("token", token, {
   httpOnly: true,
-//   secure: true,
-//   sameSite: "none",
+   secure: true,
+  sameSite: "none",
+ partitioned: true,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
